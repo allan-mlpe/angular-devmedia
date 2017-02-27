c798e5156a969191c877b5bea756d504eb1b1247
Allan Monteiro <allan_mlpe@hotmail.com>
2017-02-27 16:51:37 -0300

    Conteúdo da aula 7, com aperfeiçoamento

---
+++ aula07/js/aula07Controller.js
@@ -0,0 +1,26 @@
+app.controller('aula07Controller', function($scope) {
+
+    //o array não pode ter dados duplicados, caso contrário uma exceção será estourada
+    $scope.nomes = ["Jão", "Maria", "Jaozão", "Zeca", "Severina"];
+
+    $scope.pessoas = [];
+
+    //definimos o status false como default
+    $scope.newPessoa = { status: false};
+
+    $scope.pessoas.push(
+        {nome: "Jão", idade: 25, status: false},
+        {nome: "Jaozão", idade: 43, status: false},
+        {nome: "Maria", idade: 21, status: true}
+    );
+
+    $scope.adicionarPessoa = function() {
+        //adicionamos a nova pessoa no array
+        $scope.pessoas.push($scope.newPessoa);
+        console.dir($scope.pessoas);
+
+        //zeramos a variável newPessoa
+        $scope.newPessoa = { status: false };
+
+    };
+});
